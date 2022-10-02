4e7d98e10ae31e3d8ba831dd1d0f16012ffd825a
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-10-02 20:37:06 -0300

    add fifo component. Ref #8

---
+++ test/test_fifo.py
@@ -0,0 +1,45 @@
+import cocotb
+from cocotb.triggers import Timer
+
+
+@cocotb.test()
+async def reset(dut):
+    """Testing reset condition."""
+    dut.reset.value = 0
+
+    dut.entrant.value = 1
+
+    fifo_size = 32 + 16 + 1
+    for cycle in range(fifo_size):
+        dut.clock.value = 0
+        await Timer(1, units="ns")
+        dut.clock.value = 1
+        await Timer(1, units="ns")
+
+    dut._log.info("left_half is %s", dut.left_half.value)
+    assert dut.left_half.value == 0, "left_half is not 0!"
+
+    dut._log.info("evaluated is %s", dut.evaluated.value)
+    assert dut.evaluated.value == 0, "evaluated is not 0!"
+
+    dut._log.info("right_half is %s", dut.right_half.value)
+    assert dut.right_half.value == 0, "right_half is not 0!"
+
+
+@cocotb.test()
+async def evaluated_position(dut):
+    """Testing evaluated position in fifo"""
+    dut.reset.value = 1
+    dut.enable.value = 1
+
+    dut.entrant.value = 1
+
+    eval_pos = 16 + 8 + 1
+    for cycle in range(eval_pos):
+        dut.clock.value = 0
+        await Timer(1, units="ns")
+        dut.clock.value = 1
+        await Timer(1, units="ns")
+
+    dut._log.info("evaluated is %s", dut.evaluated.value)
+    assert dut.evaluated.value == 1, "evaluated is not 1!"
