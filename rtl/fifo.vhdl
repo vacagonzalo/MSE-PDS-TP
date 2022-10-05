76aa1a1d883c92e93f4634e68288ffd2361b040a
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-10-04 22:05:17 -0300

    add outputs to fifo. Ref #11

--- rtl/fifo.vhdl
+++ rtl/fifo.vhdl
@@ -9,36 +9,30 @@ entity fifo is
         G : integer := 8   -- CFAR guard size
     );
     port(
-        -- Common control signals -------------------------
-        clock : in std_logic;
-        reset : in std_logic;
+        -- Common control signals -------------------------------
+        clock  : in std_logic;
+        reset  : in std_logic;
         enable : in std_logic;
-        ---------------------------------------------------
+        ---------------------------------------------------------
 
-        -- Input ------------------------------------------
+        -- Input ------------------------------------------------
         entrant : in std_logic_vector(B-1 downto 0);
-        ---------------------------------------------------
+        ---------------------------------------------------------
 
-        -- Outputs -----------------------------------------
-        left_half : out std_logic_vector(B-1 downto 0);
-        evaluated : out std_logic_vector(B-1 downto 0);
-        right_half : out std_logic_vector(B-1 downto 0)
-        ---------------------------------------------------
+        -- Outputs ----------------------------------------------
+        left_half_entering  : out std_logic_vector(B-1 downto 0);
+        left_half_outgoing  : out std_logic_vector(B-1 downto 0);
+        evaluated           : out std_logic_vector(B-1 downto 0);
+        right_half_entering : out std_logic_vector(B-1 downto 0);
+        right_half_outgoing : out std_logic_vector(B-1 downto 0)
+        ---------------------------------------------------------
     );
 end fifo;
 
 architecture rtl of fifo is
 
-    constant half_size : integer := B * (N / 2);
-    constant guard_size : integer := B * G;
-    constant eval_size : integer := B * 1;
-
-    constant block_size : integer := (half_size * 2) + (guard_size * 2) + eval_size;
-
-    signal aux : std_logic_vector(block_size - 1 downto 0);
-
-    constant eval_high : integer := aux'high - half_size - guard_size;
-    constant eval_low : integer := aux'low + half_size + guard_size;
+    type memory_t is array (N + (2 * G) downto 0) of std_logic_vector(B-1 downto 0);
+    signal memory : memory_t;
 
 begin
 
@@ -46,16 +40,24 @@ begin
     begin
         if rising_edge(clock) then
             if reset = '0' then
-                aux <= (others => '0');
+                for i in (memory'left) downto (memory'right) loop
+                    memory(i) <= (others => '0');
+                end loop;
             elsif enable = '1' then
-                -- shift to the right by slicing and concatenating
-                aux <= aux(aux'high - B downto aux'low) & entrant;
+                for i in (memory'left) downto (memory'right + 1) loop
+                    memory(i-1) <= memory(i);
+                end loop;
+                memory(memory'left) <= entrant;
             end if;
         end if;
     end process sequence;
 
-    left_half <= aux(aux'high downto (aux'high - left_half'high));
-    evaluated <= aux(eval_high downto eval_low);
-    right_half <= aux((aux'low + right_half'high) downto aux'low);
+    left_half_entering  <= memory(memory'left);
+    left_half_outgoing  <= memory(memory'left - (N/2));
+
+    evaluated <= memory((memory'left + memory'right) / 2);
+
+    right_half_entering <= memory(memory'right + (N/2));
+    right_half_outgoing <= memory(memory'right);
 
 end architecture rtl;
